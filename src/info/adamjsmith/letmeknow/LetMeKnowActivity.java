a1874ff715a1104ad1166818f89732ef3773b771
Adam Smith <adam.smith18@ntlworld.com>
2014-06-16 12:11:58 +0100

    Initial commit

---
+++ src/info/adamjsmith/letmeknow/LetMeKnowActivity.java
@@ -0,0 +1,15 @@
+package info.adamjsmith.letmeknow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LetMeKnowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
