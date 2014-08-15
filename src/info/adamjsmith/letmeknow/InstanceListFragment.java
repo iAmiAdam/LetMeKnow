300e6941858ebe8d795c1ad3d5f77bc8b74b3bf3
Adam Smith <adam.smith18@ntlworld.com>
2014-08-15 19:09:54 +0100

    Instance List Fragment layout finished

--- src/info/adamjsmith/letmeknow/InstanceListFragment.java
+++ src/info/adamjsmith/letmeknow/InstanceListFragment.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
