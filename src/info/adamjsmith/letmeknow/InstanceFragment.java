f9e5bd37508946b73aeb48b1f6ffc6a2c6dd9574
Adam Smith <adam.smith18@ntlworld.com>
2014-08-15 13:40:37 +0100

    Map View in Instance Fragment

--- src/info/adamjsmith/letmeknow/InstanceFragment.java
+++ src/info/adamjsmith/letmeknow/InstanceFragment.java
@@ -10,10 +10,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+
 public class InstanceFragment extends Fragment {
 	public static final String EXTRA_INSTANCE_ID = "info.adamjsmith.letmeknow.instance_id";
 
 	private Instance mInstance;
+	private MapView mapView;
+	private GoogleMap map;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,7 +40,27 @@ public class InstanceFragment extends Fragment {
 		if (mInstance.getMessage() != null) {
 			InstanceHolder.get(getActivity()).saveInstances();
 		}
+		mapView.onPause();
 	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		mapView.onResume();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+	
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		mapView.onLowMemory();
+	}
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -39,6 +68,17 @@ public class InstanceFragment extends Fragment {
 		if(NavUtils.getParentActivityName(getActivity()) != null) {
 			getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+		
+		mapView = (MapView) v.findViewById(R.id.instance_map);
+		mapView.onCreate(savedInstanceState);
+		
+		map = mapView.getMap();
+		map.getUiSettings().setMyLocationButtonEnabled(false);
+		
+		MapsInitializer.initialize(getActivity());
+		
+		CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(52.941128, -1.260106), 10);
+		map.animateCamera(cameraUpdate);
 		
 		return v;
 	}
